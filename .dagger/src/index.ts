1637de87ae9ce4a421defd516207be55910b17c9
Jacob Hands <jacob@jacobhands.com>
2025-05-07 08:53:49 -0500

    updates

--- .dagger/src/index.ts
+++ .dagger/src/index.ts
@@ -5,18 +5,37 @@ import {
   object,
   func,
   Platform,
+  argument,
 } from "@dagger.io/dagger"
 
 @object()
 export class DaggerArmIssueRepro {
-  /**
-   * Returns a container that echoes whatever string argument is provided
-   */
+  source: Directory
+
+  constructor(
+    @argument({
+      defaultPath: "/",
+      ignore: ["**/node_modules/", "**/.turbo/", "**/dist/"],
+    })
+    source: Directory
+  ) {
+    this.source = source
+  }
+
   @func()
   build(): Container {
     return dag
       .container({ platform: "linux/amd64" as Platform })
-      .from("public.ecr.aws/debian/debian:12-slim")
-      .withExec(["mkdir", "-p", "/work/packages/workspace-dependencies/.turbo"])
+      .from("public.ecr.aws/docker/library/node:22")
+      .withExec(["npm", "install", "-g", "pnpm@10.10.0"])
+      .withWorkdir("/work")
+      .withDirectory("/work", this.source)
+      .withExec([
+        "pnpm",
+        "install",
+        "--frozen-lockfile",
+        "--child-concurrency=10",
+      ])
+      .withExec(["pnpm", "turbo", "build"])
   }
 }
